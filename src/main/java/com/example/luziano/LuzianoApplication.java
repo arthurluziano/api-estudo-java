5ecff44f9020000d02e59b68dcf1671452310c6e
Arthur Luziano <luzianoarthur@gmail.com>
2025-01-24 21:03:33 -0300

    Primeiro commit.

---
+++ src/main/java/com/example/luziano/LuzianoApplication.java
@@ -0,0 +1,13 @@
+package com.example.luziano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuzianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuzianoApplication.class, args);
+	}
+
+}
